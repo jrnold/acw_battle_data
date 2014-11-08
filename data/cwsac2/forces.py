f5a32b9970e50161b81a825f6f2cbb160ea2d500
Jeff Arnold <jeffrey.arnold@gmail.com>
2014-11-07 18:48:55 -0800

    split bodart into csv tables

--- data/cwsac2/forces.py
+++ data/cwsac2/forces.py
@@ -1,17 +1,29 @@
-import json
 import yaml
+import csv
 
 with open("forces.yaml", "r") as f:
     data = yaml.load(f)
 
-allkeys = {}    
+allkeys = set()
 for k, v in data.items():
-    for combatant in ('US', 'CS', 'I'):
+    for combatant in ('CS', 'US', 'I'):
         if combatant in v:
             for j in v[combatant].keys():
-                allkeys[j] = None
+                allkeys.add(j)
 
-for x in sorted([k for k in allkeys.keys()]):
-    print(x)
+varnames = sorted(list(allkeys))
+varnames = [x for x in varnames if x != 'description']
 
+with open('forces.csv', 'w') as f:
+    writer = csv.DictWriter(f, ['cwsac', 'combatant', 'forces_text', 'description'] + varnames)
+    print(writer.fieldnames)
+    writer.writeheader()
+    for k, v in data.items():
+        for combatant in ('CS', 'US', 'I'):
+            if combatant in v:
+                row = v[combatant]
+                row['cwsac'] = k
+                row['combatant'] = combatant
+                row['forces_text'] = v['forces_text']
+                writer.writerow(row)
 
