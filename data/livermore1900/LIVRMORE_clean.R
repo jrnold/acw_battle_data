7bd9c174930f2f6f75534d0f206e3a389289e00c
Jeff Arnold <jeffrey.arnold@gmail.com>
2014-11-22 12:56:49 -0800

    cleaned up LIVRMORE data

---
+++ data/livermore1900/LIVRMORE_clean.R
@@ -0,0 +1,71 @@
+#'
+#' Clean the data in PAR/data/LIVRMORE.csv and split it into
+#' battle and force tables
+#'
+library("dplyr")
+library("stringr")
+
+read_csv <- function(x, ...) {
+  tbl_df(read.csv(x, ..., stringsAsFactors = FALSE,
+                  check.names = FALSE))
+}
+
+write_csv <- function(x, ...) {
+  write.csv(x, ..., na = "", row.names = FALSE)
+}
+
+LIV_BATTLES_FILE <- "../PAR/data/LIVRMORE.csv"
+livrmore<- read_csv(LIV_BATTLES_FILE)
+
+liv_battles <-
+  (livrmore %>%
+   select(seq_no, battle_name, start_date, end_date, win_side))
+
+strcas_vars <- c("str", "kia")
+attacker_vars <- str_c("attacker_", strcas_vars)
+attacker_vars <- str_c("defender_", strcas_vars)
+  
+union_attacker <- 
+  (livrmore
+   %>% filter(attacker == "UNION")
+  )
+union_attacking_forces <-
+  (union_attacker
+   %>% select(seq_no, starts_with("attacker_"))
+   %>% mutate(belligerent = "UNION", attacker = TRUE))
+names(union_attacking_forces) <-
+  gsub("attacker_", "", names(union_attacking_forces))
+
+confed_defending_forces <-
+  (union_attacker
+   %>% select(seq_no, starts_with("defender_"))
+   %>% mutate(belligerent = "CONFEDERATE", attacker = FALSE)
+  )
+names(confed_defending_forces) <-
+  gsub("defender_", "", names(confed_defending_forces))
+
+confed_attacker <- 
+  (livrmore
+   %>% filter(attacker == "CONFEDERATE")
+  )
+confed_attacking_forces <-
+  (confed_attacker
+   %>% select(seq_no, starts_with("attacker_"))
+   %>% mutate(belligerent = "CONFEDERATE", attacker = TRUE))
+names(confed_attacking_forces) <-
+  gsub("attacker_", "", names(confed_attacking_forces))
+
+union_defending_forces <-
+  (confed_attacker
+   %>% select(seq_no, starts_with("defender_"))
+   %>% mutate(belligerent = "UNION", attacker = FALSE)
+  )
+names(union_defending_forces) <-
+  gsub("defender_", "", names(union_defending_forces))
+
+liv_forces <-
+  rbind(union_attacking_forces, confed_defending_forces,
+      confed_attacking_forces, confed_defending_forces)
+
+write_csv(liv_battles, file = "LIVRMORE_battles.csv")
+write_csv(liv_forces, file = "LIVRMORE_forces.csv")
