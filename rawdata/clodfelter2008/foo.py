086cbab44d502ebf9987ae175f25718ecb14c583
jrnold <jeffrey.arnold@gmail.com>
2015-12-06 23:33:45 -0800

    misc

--- rawdata/clodfelter2008/foo.py
+++ rawdata/clodfelter2008/foo.py
@@ -5,30 +5,10 @@ from os import path
 with open(path.expanduser("clodfelter.yaml"), "r") as f:
     data = yaml.load(f)
 
-commanders = {}
-with open("../../data/nps_commanders.csv", "r") as f:
-    for row in csv.DictReader(f):
-        if row['belligerent'] == "Native American":
-            continue
-        if row['cwsac_id'] not in commanders:
-            commanders[row['cwsac_id']] = {'US': [],
-                                         'Confederate': []}
-        keys = ('PersonID', 'last_name', 'first_name',
-                'middle_name', 'middle_initial', 'rank', 'navy')
-        d = dict((k, row[k]) for k in keys)
-        commanders[row['cwsac_id']][row['belligerent']].append(d)
-
 for battle in data:
-    cwsac = None
-    if 'cwsac' in battle:
-        for x in battle['cwsac']:
-            if x['relation'] == "=":
-                v = commanders[x['id']]
-                battle['forces']['US']['commanders'] = v['US']
-                battle['forces']['Confederate']['commanders'] = v['Confederate']
-                continue
-    else:
-        print(battle['battle'])        
+    for k in battle["forces"]:
+        for cdr in battle["forces"][k]['commanders']:
+            cdr['navy'] = (cdr['navy'] == '1')
 
 with open("clodfelter-new.yaml", "w") as f:
     yaml.dump(data, f, default_flow_style = False)
