5d20b3fcb31916f60bc484d3f60b2a3b3cc83233
jrnold <jeffrey.arnold@gmail.com>
2015-12-07 06:34:37 -0800

    misc

--- rawdata/metadata/foo.py
+++ rawdata/metadata/foo.py
@@ -4,11 +4,11 @@ import fnmatch
 from os import path
 
 for fn in os.listdir("resources"):
-    if fnmatch.fnmatch(fn, "thorpe_*.yaml"):
+    if fnmatch.fnmatch(fn, "dyer_*.yaml"):
         print(fn)
         with open(path.join("resources", fn), "r") as f:
             data = yaml.load(f)
-        data['sources'] = ["Thorpe2011"]
+        data['sources'] = list(set(data['sources'] + ['DyerBattles']))
         with open(path.join("resources", fn), "w") as f:
             data = yaml.dump(data, f, default_flow_style = False, allow_unicode = True)
         
