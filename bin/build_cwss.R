e34fff0997386fa3660b23eedb7e183171ee347b
jrnold <jeffrey.arnold@gmail.com>
2015-12-10 06:18:26 -0800

    misc

---
+++ bin/build_cwss.R
@@ -0,0 +1,33 @@
+library("dplyr")
+library("stringr")
+library("magrittr")
+
+src <- "."
+
+path <- file.path("rawdata/cwss/nps_cwss.sqlite3")
+nps_cwss <- src_sqlite(path)
+
+regiments_unitz <- tbl(nps_cwss, "Regiments_Unitz") %>%
+  select(REG_UNIT_CODE,
+         REG_SIDE,
+         REG_STATE,
+         REG_ORDINAL,
+         REG_TYPE,
+         REG_SPECIAL,
+         REG_DUPLICATE,
+         REG_ETHNIC) %>%
+  collect() %>%
+  filter(! str_detect(REG_UNIT_CODE, regex("^admin", ignore = TRUE))) %>%
+  setNames(tolower(gsub("REG_", "", names(.))))
+
+unit_titles <-
+  bind_rows(collect(tbl(nps_cwss, "Unititle")),
+            collect(tbl(nps_cwss, "Contitle"))) %>%
+  select(STATE, SIDE, TITLE) %>%
+  setNames(tolower(names(.)))
+
+state_name <-
+  tbl(nps_cwss, "State_Name") %>%
+  select(STATE_ABBR, STATE_NAME) %>%
+  setNames(tolower(names(.)))
+
---
+++ rawdata/metadata/resources/nps_people.rst
@@ -0,0 +1,5 @@
+People data for the combined National Park Services data.
+
+This data combining the National Park Services reports: Shenandoah, preliminary CWSAC reports in the AAD (1990-1993), CWSAC Report battle summaries (1997), Civil War Soldiers and Sailors (CWSS) database, and the CWSAC Report Updates (2009-2013).
+This dataset is derived from :doc:`cwss_people` and supplemented with commanders appearing in CWSAC and other battles that did not appear in that data.
+
