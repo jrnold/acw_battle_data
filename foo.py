0fe225cb8c25144800b0c4da2d0b2510d4c3c362
jrnold <jeffrey.arnold@gmail.com>
2016-03-12 16:46:37 -0800

    added cwsac -> bodart mappings

--- foo.py
+++ foo.py
@@ -10,32 +10,29 @@ import yaml
 #
 #fox = pd.read_csv("data/fox_forces2.csv")
 
-liv2cwsac = pd.read_csv("data/livermore_to_cwsac.csv")
-liv = pd.read_csv("data/livermore_battles.csv")
+bod2cwsac = pd.read_csv("data/bodart1908_to_cwsac.csv")
+bod = pd.read_csv("data/bodart1908_battles.csv")
 cwsac = pd.read_csv("data/nps_battles.csv")
 
 
 results = []
-for row in liv2cwsac.itertuples():
-    print(row)
-    liv_id = int(row[1])
+for row in bod2cwsac.itertuples():
+    _id = int(row[1])
     cwsac_id = row[2]
     cwsac_battle = cwsac[cwsac.cwsac_id == cwsac_id].iloc[0]
     cwsac_description = ('%s , %s (%s - %s)' % (cwsac_battle['battle_name'],
                                                 cwsac_battle['state'],
                                                 cwsac_battle['start_date'],
                                                 cwsac_battle['end_date'],))
-    liv_battle = liv[liv.par_id == liv_id].iloc[0]
-    liv_description = ('%s , %s (%s - %s)' % (liv_battle['battle_name'],
-                                                liv_battle['state'],
-                                                liv_battle['start_date'],
-                                                liv_battle['end_date'],))
-                                                
+    battle = bod[bod.battle_id == _id]
+    description = ('%s (%s - %s)' % (battle.iloc[0,:]['name'],
+                                                battle.iloc[0,:]['start_date'],
+                                                battle.iloc[0,:]['end_date'],))                                               
     results.append({'cwsac': [{'id': cwsac_id, 'description': cwsac_description}],
-                    'livermore': [{'id': liv_id, 'description': liv_description}],
+                    'phisterer': [{'id': liv_id, 'description': description}],
                     'start_date': cwsac_battle['start_date'],
                     'relation': row[3]})
                   
 results.sort(key = lambda x: x['start_date'])
-with open('cwsac_to_livermore.csv', 'w') as f:
+with open('cwsac_to_bodart.csv', 'w') as f:
     yaml.dump(results, f, default_flow_style = False)
