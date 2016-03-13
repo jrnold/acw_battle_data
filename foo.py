adfd50360a9184f30bd46c03aef1393b99b39fd5
jrnold <jeffrey.arnold@gmail.com>
2016-03-12 16:57:39 -0800

    add phisterer

--- foo.py
+++ foo.py
@@ -10,29 +10,31 @@ import yaml
 #
 #fox = pd.read_csv("data/fox_forces2.csv")
 
-bod2cwsac = pd.read_csv("data/bodart1908_to_cwsac.csv")
-bod = pd.read_csv("data/bodart1908_battles.csv")
+phi2cwsac = pd.read_csv("data/phisterer_to_cwsac.csv")
+phi = pd.read_csv("data/phisterer_battles.csv")
 cwsac = pd.read_csv("data/nps_battles.csv")
 
 
 results = []
-for row in bod2cwsac.itertuples():
-    _id = int(row[1])
+for row in phi2cwsac.itertuples():
+    print(row)
+    _id = row[1]
     cwsac_id = row[2]
     cwsac_battle = cwsac[cwsac.cwsac_id == cwsac_id].iloc[0]
     cwsac_description = ('%s , %s (%s - %s)' % (cwsac_battle['battle_name'],
                                                 cwsac_battle['state'],
                                                 cwsac_battle['start_date'],
                                                 cwsac_battle['end_date'],))
-    battle = bod[bod.battle_id == _id]
-    description = ('%s (%s - %s)' % (battle.iloc[0,:]['name'],
+    battle = phi[phi.battle == _id]
+    description = ('%s, %s (%s - %s)' % (battle.iloc[0,:]['battle_name'],
+                                                battle.iloc[0,:]['state'],
                                                 battle.iloc[0,:]['start_date'],
                                                 battle.iloc[0,:]['end_date'],))                                               
     results.append({'cwsac': [{'id': cwsac_id, 'description': cwsac_description}],
-                    'phisterer': [{'id': liv_id, 'description': description}],
+                    'phisterer': [{'id': int(_id), 'description': description}],
                     'start_date': cwsac_battle['start_date'],
                     'relation': row[3]})
                   
 results.sort(key = lambda x: x['start_date'])
-with open('cwsac_to_bodart.csv', 'w') as f:
+with open('cwsac_to_phisterer.csv', 'w') as f:
     yaml.dump(results, f, default_flow_style = False)
