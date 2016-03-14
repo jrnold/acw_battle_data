04d1cc322ed0c98251b447e0c6c14fb62bf39de3
jrnold <jeffrey.arnold@gmail.com>
2016-03-13 17:18:28 -0700

    editing Bodart raw data

--- rawdata/bodart1908/foo.py
+++ rawdata/bodart1908/foo.py
@@ -5,9 +5,4 @@ with open('bodart.yaml', 'r') as f:
     data = yaml.load(f)
 
 for battle in data:
-    if 'cwsac' in battle:
-        ids = [x['uri'] for x in battle['cwsac']]
-        battle['cwsac'] = {'ids': ids, 'relation': 'eq'}
-
-with open('bodart_new.yaml', 'w') as f:
-    yaml.dump(data, f, default_flow_style = False)
+    print("%s (%s - %s)" % (battle['battle_name'], battle['start_date'], battle['end_date']))
