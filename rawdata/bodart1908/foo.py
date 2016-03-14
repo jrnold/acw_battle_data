3b81f7a236aa19d6c0adbc35e8f11dfcf19844c8
jrnold <jeffrey.arnold@gmail.com>
2016-03-13 17:28:27 -0700

    update raw bodart data
    - order by appearance in Bodart data. Add battle_id number
    - change dbpedia and cwsac fields to be dictionaries with links and relation.

--- rawdata/bodart1908/foo.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-with open('bodart.yaml', 'r') as f:
-    data = yaml.load(f)
-
-for battle in data:
-    print("%s (%s - %s)" % (battle['battle_name'], battle['start_date'], battle['end_date']))
