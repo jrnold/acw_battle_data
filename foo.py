075a3fadedda1df17a226bf40963edaba3a8bc67
jrnold <jeffrey.arnold@gmail.com>
2016-03-17 22:30:09 -0700

    misc

--- foo.py
+++ foo.py
@@ -1,3 +1,7 @@
 import yaml
-import json
-import csv
+
+with open('rawdata/kennedy1997/kennedy1997.yaml', 'r') as f:
+  data = yaml.load(f)
+
+for battle in data:
+  battle['state'] = battle['battle_id'][:2]
