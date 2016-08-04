80d08f1d2cbb95e92fbf9cbe49150afb4ef783aa
jrnold <jeffrey.arnold@gmail.com>
2016-08-04 16:58:18 -0700

    Use only python for updating Eicher
    - Consolidate the updating of Eicher into a single python script
    - Minor edits to the metadata

--- bin/build_eicher.py
+++ bin/build_eicher.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
+import csv
 import sys
 import os.path as path
 import shutil
 
 def build(src, dst):
-    outfile = "eicher_chronology.csv"
-    print("writing %s" % outfile)
-    shutil.copy(path.join(src, "rawdata", "eicher", "chronology.csv"),
-                path.join(dst, outfile))
+    with open(path.join(src, "rawdata", "eicher", "chronology.csv"), 'r') as f:
+        data = [row for row in csv.DictReader(f)]
+    for row in data:
+        if row['superevent'] is None:
+            row['superevent'] = row['event']
+    outfile = path.join(dst, "eicher_chronology.csv")
+    print("Writing to %s" % dst)
+    with open(outfile, 'w') as f:
+        writer = csv.DictWriter(f, ('date', 'event', 'superevent', 'subevent'))
+        writer.writeheader()
+        writer.writerows(data)
 
 def main():
     src, dst = sys.argv[1:3]
