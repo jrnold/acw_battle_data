acfab2f41f623305a5153b45605c82438b6ec433
jrnold <jeffrey.arnold@gmail.com>
2017-10-23 22:57:01 -0700

    misc

--- bin/build_fox.py
+++ bin/build_fox.py
@@ -1,20 +1,23 @@
-import sys
-import os.path as path
 import json
+import os.path as path
+import sys
 
 import pandas as pd
 import yaml
+
 
 def build_forces(src, dst):
     data = pd.read_csv(src)
     data['from_footnote'] = data['battle_id'].str.match('[UC][1-9]+[A-Z]').\
         astype(int)
-    data.to_csv(dst, index = False)
+    data.to_csv(dst, index=False)
     print("wrote %s" % dst)
 
+
 def build_outcomes(src, dst):
-    pd.read_csv(src).to_csv(dst, index = False)
+    pd.read_csv(src).to_csv(dst, index=False)
     print("wrote %s" % dst)
+
 
 def build_to_cwsac(src, dst):
     with open(src, 'r', encoding='utf-8') as f:
@@ -26,15 +29,19 @@ def build_to_cwsac(src, dst):
         json.dump(data, f)
         print("wrote %s" % dst)
 
+
 def main():
     src, dst = sys.argv[1:3]
     srcdir = path.join(src, 'rawdata', 'fox1898')
-    build_outcomes(path.join(srcdir, "fox_outcomes.csv"),
-              path.join(dst, "fox_outcomes.csv"))
-    build_forces(path.join(srcdir, 'fox_forces.csv'),
-                 path.join(dst, 'fox_forces.csv'))
-    build_to_cwsac(path.join(srcdir, 'fox_forces_to_cwsac.yaml'),
-                    path.join(dst, 'fox_forces_to_cwsac.json'))
+    build_outcomes(
+        path.join(srcdir, "fox_outcomes.csv"),
+        path.join(dst, "fox_outcomes.csv"))
+    build_forces(
+        path.join(srcdir, 'fox_forces.csv'), path.join(dst, 'fox_forces.csv'))
+    build_to_cwsac(
+        path.join(srcdir, 'fox_forces_to_cwsac.yaml'),
+        path.join(dst, 'fox_forces_to_cwsac.json'))
+
 
 if __name__ == '__main__':
     main()
--- bin/build_nps_combined.R
+++ bin/build_nps_combined.R
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env Rscript
 source("R/misc.R")
 
